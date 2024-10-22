d518895bc5f1336dd6be428c255aa66e15f5a706
mkreynolds919 <mkreynolds919@gmail.com>
2024-10-22 13:29:13 -0500

    Add table

--- myscripts.js
+++ myscripts.js
@@ -14,24 +14,24 @@ function addToLibrary(book) {
     myLibrary.push(book);
 }
 
-
 const book1 = new Book("First Book", "John Smith", 592);
 const book2 = new Book("Second Book", "Smith Johnson", 486);
 
 addToLibrary(book1);
 addToLibrary(book2);
 
-//creates and styles header div//
-const libraryHeader = document.createElement("div");
-libraryHeader.textContent = "My Library";
-libraryHeader.style.fontSize = "max(18px, 1.2em";
-libraryHeader.style.textAlign = "center";
-document.body.appendChild(libraryHeader);
+const table = document.getElementById("table");
 
-//creates the card for each book in the myLibrary list//
+function addBookToTable(book) {
+    const row = table.insertRow();
+    const title = row.insertCell();
+    title.textContent = book.title;
+    const author = row.insertCell();
+    author.textContent = book.author;
+    const pages = row.insertCell();
+    pages.textContent = book.pages;
+}
+
 for (var i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement("div");
-    const book = myLibrary[i];
-    card.textContent = `Title: ${book.title}`;
-    document.body.appendChild(card);
+    addBookToTable(myLibrary[i]);
 }
